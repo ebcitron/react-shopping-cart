f667629b3f58216e83b2f013541bc07be3431f87
Eli Citron <ebcitron@gmail.com>
2019-08-12 16:17:10 -0700

    Re-formatted

--- src/App.js
+++ src/App.js
@@ -7,38 +7,46 @@ import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 
+//Context
+import ProductContext from './contexts/ProductContext';
+import CartContext from './contexts/CartContext';
+
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
 
 	const addItem = item => {
 		setCart([...cart, item]);
-	const IBIKI = 666;
 	}
-	;
+		;
 
 	return (
-		<div className="App">
-			<Navigation cart={cart} />
+		<ProductContext.Provider value={{ products, addItem }}>
+			<CartContext.Provider value={cart} >
 
-			{/* Routes */}
-			<Route
-				exact
-				path="/"
-				render={() => (
-					<Products
-						products={products}
-						addItem={addItem}
-					/>
-				)}
-			/>
+				<div className="App">
 
-			<Route
-				path="/cart"
-				render={() => <ShoppingCart cart={cart} />}
-			/>
-		</div>
+					<Navigation cart={cart} />
+
+					{/* Routes */}
+					<Route
+						exact
+						path="/"
+						component={Products} />
+
+					<Route
+						path="/cart"
+						component={cart} />
+				</div>
+			</CartContext.Provider>
+		</ProductContext.Provider>
 	);
 }
 
 export default App;
+
+
+//   Eli
+//   Squid
+//   Ichigo
+//   Bankai
